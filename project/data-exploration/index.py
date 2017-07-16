f21a1f80ea4660fe30f312bfc1e5987867cb342c
Matthew Bozelka <mattbozelka.dev@gmail.com>
2017-07-15 23:35:27 -0700

    New Feature: Data Exploration
    Added a Jupyter Notebook to begin exploring my data set needed to build my model

---
+++ project/data-exploration/index.py
@@ -0,0 +1,14 @@
+
+#################################
+# Main file to begin program 
+#################################
+import pandas as pd
+import numpy as np
+
+
+def test_run():
+    print 'we are running from main'
+
+
+if __name__ == "__main__":
+    test_run()
